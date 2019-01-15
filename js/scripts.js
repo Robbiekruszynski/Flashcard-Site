22469d9589e94d66b167c9a27813bfe1f4aa9830
Dino Nguyen and Robbie Kruszynski <robbie@>
2019-01-15 15:02:38 -0800

    Add flashcard functionality - ID toggle and gif background.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,51 @@
 $(document).ready(function() {
-  $(".click").click(function() {
-    $(".blank").toggle();
-    $(".info").toggle();
+  $(".click1").click(function() {
+    $("#blank1").toggle();
+    $("#info1").toggle();
   });
-  $(".click").click(function() {
-    $(".blank2").toggle();
-    $(".info2").toggle();
+  $(".click2").click(function() {
+    $("#blank2").toggle();
+    $("#info2").toggle();
   });
+  $(".click3").click(function() {
+    $("#blank3").toggle();
+    $("#info3").toggle();
+  });
+  $(".click4").click(function() {
+    $("#blank4").toggle();
+    $("#info4").toggle();
+  });
+  $(".click5").click(function() {
+    $("#blank5").toggle();
+    $("#info5").toggle();
+  });
+  $(".click6").click(function() {
+    $("#blank6").toggle();
+    $("#info6").toggle();
+  });
+  $(".click7").click(function() {
+    $("#blank7").toggle();
+    $("#info7").toggle();
+  });
+  $(".click8").click(function() {
+    $("#blank8").toggle();
+    $("#info8").toggle();
+  });
+  $(".click9").click(function() {
+    $("#blank9").toggle();
+    $("#info9").toggle();
+  });
+  $(".click10").click(function() {
+    $("#blank10").toggle();
+    $("#info10").toggle();
+  });
+  $(".click11").click(function() {
+    $("#blank11").toggle();
+    $("#info11").toggle();
+  });
+  $(".click12").click(function() {
+    $("#blank12").toggle();
+    $("#info12").toggle();
+  });
+
 });
