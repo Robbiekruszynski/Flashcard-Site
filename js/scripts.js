2e45b06d7d9c4433a1b32631e3e946ef330d6326
Dino Nguyen and Robbie Kruszynski <robbie@>
2019-01-15 14:15:58 -0800

    Add flashcard content and flip all cards function.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".click").click(function() {
+    $(".blank").toggle();
+    $(".info").toggle();
+  });
+  $(".click").click(function() {
+    $(".blank2").toggle();
+    $(".info2").toggle();
+  });
+});
